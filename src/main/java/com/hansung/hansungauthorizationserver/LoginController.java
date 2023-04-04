7eb9c44f7e731c850aee81de4d7cca966734bf0a
htkwon <a3001sd@gmail.com>
2023-04-04 12:43:14 +0900

    로그아웃 API 코드 변경

--- src/main/java/com/hansung/hansungauthorizationserver/LoginController.java
+++ src/main/java/com/hansung/hansungauthorizationserver/LoginController.java
@@ -1,7 +1,20 @@
 package com.hansung.hansungauthorizationserver;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 @Controller
 public class LoginController {
@@ -9,4 +22,24 @@ public class LoginController {
     public String login() {
         return "/login";
     }
+
+    
+    @GetMapping("/logout")
+    public void logout(HttpSession session, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        session.invalidate();
+        final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request,response,auth);
+            auth.setAuthenticated(false);
+            SecurityContextHolder.clearContext();
+            SecurityContextHolder.getContext().setAuthentication(null);
+        }
+        Cookie cookie = new Cookie("JSESSIONID", null);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        response.sendRedirect("http://localhost:8070/login");
+    }
+
+
 }
