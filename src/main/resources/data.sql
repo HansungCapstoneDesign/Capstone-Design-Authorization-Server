dfec487a3982256fbae0026dc88d60ab0e7dbd81
Chan-GN <ocg0229@gmail.com>
2023-04-06 14:24:45 +0900

    FEAT : 권한 관련 data.sql 수정
    - read -> ROLE_STUDENT로 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,7 +6,7 @@ VALUES
 INSERT INTO `users`
     (`id`, `username`, `password`, `authority`, `student_id`, `first_track`, `second_track`, `picture`)
 VALUES
-    ('1', '오찬근', '1234', 'read', '1891239', '모바일소프트웨어트랙', '웹공학트랙', 'http://localhost:8081/profile_image/picture.jpg'),
-    ('2', '권현택', '1234', 'read', '1871308', '모바일소프트웨어트랙', '웹공학트랙', 'http://localhost:8081/profile_image/picture.jpg'),
-    ('3', '곽은서', '1234', 'read', '1971096', '모바일소프트웨어트랙', '웹공학트랙', 'http://localhost:8081/profile_image/picture.jpg'),
-    ('4', '김서영', '1234', 'read', '2071274', '모바일소프트웨어트랙', '웹공학트랙', 'http://localhost:8081/profile_image/picture.jpg');
+    ('1', '오찬근', '1234', 'ROLE_STUDENT', '1891239', '모바일소프트웨어트랙', '웹공학트랙', 'http://localhost:8081/profile_image/picture.jpg'),
+    ('2', '권현택', '1234', 'ROLE_STUDENT', '1871308', '모바일소프트웨어트랙', '웹공학트랙', 'http://localhost:8081/profile_image/picture.jpg'),
+    ('3', '곽은서', '1234', 'ROLE_STUDENT', '1971096', '모바일소프트웨어트랙', '웹공학트랙', 'http://localhost:8081/profile_image/picture.jpg'),
+    ('4', '김서영', '1234', 'ROLE_STUDENT', '2071274', '모바일소프트웨어트랙', '웹공학트랙', 'http://localhost:8081/profile_image/picture.jpg');
